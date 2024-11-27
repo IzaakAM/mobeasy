82dd600e3654eca5486a36095c81a1bb847f3478
IzaakAM <aubert.mecibah@gmail.com>
2024-11-27 11:38:59 +0100

    Initial commit

---
+++ src/main/java/com/mobeasy/api/MobeasyApiApplication.java
@@ -0,0 +1,13 @@
+package com.mobeasy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobeasyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobeasyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobeasy/api/MobeasyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobeasy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobeasyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
